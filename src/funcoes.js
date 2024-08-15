3f18bf32e5ecb9b2af12f493295e7ccee1fd3685
jeanarthur <jean.arthursa@gmail.com>
2024-08-14 22:39:12 -0300

    ajusta funcoes

--- src/funcoes.js
+++ src/funcoes.js
@@ -4,18 +4,17 @@ async function adicionarPontos(equipe, pontos){
     let pontuacoes = await obterPontuacoes();
     
     try {
-        const index = pontuacoes.map(elem => elem.equipe).indexOf(equipe);
+        const temEquipe = Object.keys(pontuacoes).includes(equipe);
 
-        if (index === -1){
-            pontuacoes.push({
-                equipe: equipe,
-                pontuacao: pontos
-            })
+        if (temEquipe){
+            pontuacoes[equipe].pontuacao += pontos;
         } else {
-            pontuacoes[index].pontuacao += pontos;
+            pontuacoes[equipe] = {
+                pontuacao: pontos
+            }
         }
 
-        fs.writeFile('./pontuacao.json', JSON.stringify(pontuacoes));
+        await fs.writeFile('./pontuacao.json', JSON.stringify(pontuacoes));
 
     } catch (erro) {
         console.error(`Erro ao escrever arquivo: ${erro}`);
@@ -24,7 +23,10 @@ async function adicionarPontos(equipe, pontos){
 
 async function gerarClassificacao() {
     let pontuacoes = await obterPontuacoes();
-    return pontuacoes.sort(function(a,b) {
+    pontuacoes.equipes = Object.keys(pontuacoes)
+        .map(elem => { return { equipe: elem, pontuacao: pontuacoes[elem].pontuacao } });
+
+    return pontuacoes.equipes.sort(function(a,b) {
         return a.pontuacao < b.pontuacao ? 1 : a.pontuacao > b.pontuacao ? -1 : 0;
     });
 }
@@ -39,7 +41,11 @@ async function obterPontuacoes(){
     }
 }
 
-adicionarPontos('xpto', 1);
-adicionarPontos('xyz', 3);
-gerarClassificacao()
-    .then(res => console.log(res));
+// Teste de adição e classificação de equipes
+async function executar(){
+    await adicionarPontos('xpto', 1);
+    await adicionarPontos('xyz', 3);
+    console.log(await gerarClassificacao());
+}
+
+executar();
