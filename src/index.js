df0f7f8b113dc5351ae9b8b0671748240e27fc32
Alyssonjff <alyssonjfranca@gmail.com>
2024-08-21 21:46:53 -0300

    Atualiza rotas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require('path');
-
+const funcoes = require('./funcoes')
 const app = express();
 
 app.get('/', async (req, res) => {
@@ -46,6 +46,30 @@ app.get('/background.png', async (req, res) => {
             console.log('Sent:', fileName);
         }
     });
+});
+
+app.get('/formulario', async (req, res) => {
+    const options = {
+        root: path.join('./')
+    };
+ 
+    const fileName = 'form.html';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            console.error('Error sending file:', err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+});
+
+app.get('/enviar-formulario', async (req, res) => {
+
+    const team = req.query.team;
+    const score = req.query.score;
+    funcoes.adicionarPontos(team,score)
+
+    res.send(`A equipe ${team} marcou ${score}`) 
 });
 
 app.listen(3000, ()=>{console.log("Servidor rodando...")});
