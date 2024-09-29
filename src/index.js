75007f4b799380fd1bc6fbca15166f809e2e3fe1
Alysson <104224074+Alyssonjff@users.noreply.github.com>
2024-09-29 18:34:10 -0300

    add app.POST on index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const { adicionarPontos, gerarClassificacao } = require('./funcoes.js')
 
-app.use(express.static('./src/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
     const options = {
         root: path.join('./src')
     };
- 
+
     const fileName = 'index.html';
     res.sendFile(fileName, options, function (err) {
         if (err) {
@@ -24,11 +26,41 @@ app.get('/status', (req, res) => {
     res.send('OK');
 })
 
+app.get('/output.css', async (req, res) => {
+    const options = {
+        root: path.join('./src')
+    };
+
+    const fileName = 'output.css';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            console.error('Error sending file:', err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+});
+
+app.get('/background.png', async (req, res) => {
+    const options = {
+        root: path.join('./src')
+    };
+
+    const fileName = 'background.png';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            console.error('Error sending file:', err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+});
+
 app.get('/formulario', async (req, res) => {
     const options = {
         root: path.join('./src')
     };
- 
+
     const fileName = 'form.html';
     res.sendFile(fileName, options, function (err) {
         if (err) {
@@ -39,14 +71,30 @@ app.get('/formulario', async (req, res) => {
     });
 });
 
-app.get('/enviar-formulario', async (req, res) => {
+app.post('/enviar-formulario', (req, res) => {
+    const team = req.body.team;
+    const score = req.body.score;
+    adicionarPontos(team, score);
 
-    const team = req.query.team;
-    const score = req.query.score;
-    adicionarPontos(team,score)
+    fs.readFile('pontuacao.json', (err, data) => {
+        let pontuacoes = [];
 
-    res.send(`A equipe ${team} marcou ${score} pontos`) 
+        if (!err) {
+            pontuacoes = JSON.parse(data);
+        }
+
+        pontuacoes.push({ team, score });
+
+        fs.writeFile('pontuacao.json', JSON.stringify(pontuacoes, null, 2), (err) => {
+            if (err) {
+                console.error('Erro ao salvar pontuações:', err);
+                return res.status(500).send('Erro ao salvar pontuações.');
+            }
+            res.send(`A equipe ${team} marcou ${score} pontos`);
+        });
+    });
 });
+
 
 app.listen(3000, ()=>{
     console.log("Servidor rodando...");
